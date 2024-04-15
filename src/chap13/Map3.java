0daab3ddf734f0280df7cc8a91cf70f5171f6132
chickenmin <kkjm1021@naver.com>
2024-04-15 14:36:52 +0900

    first commit

--- src/chap13/Map3.java
+++ src/chap13/Map3.java
@@ -1,12 +1,17 @@
 package chap13;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 /*
 * TreeMap : 키순으로 정렬됨*/
 public class Map3 {
     public static void main(String[] args) {
-        TreeMap<Integer,String> scores = new TreeMap<>();
+        TreeMap<Integer,String> scores = new TreeMap<>(Comparator.reverseOrder());
+//        SortedMap<Integer,String> scores = new TreeMap<Integer,String>();
+
         scores.put(87,"홍길동");
         scores.put(98,"이몽룡");
         scores.put(75,"임꺽정");
@@ -22,5 +27,9 @@ public class Map3 {
         System.out.println("마지막 entry:" + scores.lastEntry());
         System.out.println("최고 점수를 받은 사람:"+scores.get(scores.lastKey()));
         System.out.println("최고 점수를 받은 사람:"+ scores.lastEntry().getValue());
+        System.out.println("94점 앞 점수:" + scores.lowerEntry(94));
+        System.out.println("94점 뒤 점수:" + scores.higherEntry(94));
+        System.out.println("94점 포함 앞 점수:" + scores.floorEntry(95));
+        System.out.println("94점 포함 뒤 점수:" + scores.ceilingEntry(95));
     }
 }
